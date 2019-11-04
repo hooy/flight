8d7cd63bc186ea3f21c120c3f98866bb89569921
Sergejs Gorbunovs <s.gorbunovs@eco-fin.eu>
2019-11-04 16:20:38 +0200

    synced

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,11 @@
-FROM python:3
+# FROM python:3
+# ADD flight_search.py /flight_search.py
+
+
+# CMD [ "python", "./flight_search.py" ]
+
+FROM pypy:3
+
 ADD flight_search.py /flight_search.py
 
-
-CMD [ "python", "./flight_search.py" ]
-
+CMD [ "pypy3", "./flight_search.py" ]
--- flight_search.py
+++ flight_search.py
@@ -29,12 +29,38 @@ class fare(object):
 def parse_fares(json_fares):
     return [fare(**f) for f in json_fares]
 
+def find_optimal_combination_kruskal(path, fares):
+    tree_set = {}
+
+def find_optimal_combination_iterative(path, fares):
+    minimal_route = (None, Infinity())
+
+    for f1 in fares:
+        current_path, current_price, current_fares = set(f1.routes), f1.price, {f1}
+        other_fares = fares.copy()
+        other_fares.remove(f1)
+        for f2 in other_fares:
+            routes = set(f2.routes)
+            if (routes.intersection(current_path)):  # cannot take fare, contains edge we already took
+                pass
+            else:
+                current_path = current_path.union(routes)
+                current_price += f2.price
+                current_fares.update({f2})
+                if not(path - current_path): # finished route
+                    break
+        if not(path - current_path) and current_price < minimal_route[1]:
+            minimal_route = (current_fares, current_price)
+
+    return minimal_route
+
 
 def find_optimal_combination(path, fares):
 
     def fare_taker(current_path, available_fares, fares_taken, price):
+        global current_optimal
         # print(current_path, available_fares, fares_taken, price)
-        # time.sleep(2)
+        # time.sleep(1)
         if (path - current_path):  # itineary is not finished
             fl = set(available_fares - fares_taken)  # fares left
             if fl:
@@ -70,9 +96,11 @@ def main():
 
     # filter fares with extra flights
     fares = set(filter(lambda x: not (set(x.routes) - itinerary), fares))
-    fares_taken, price = find_optimal_combination(itinerary, fares)
+    # fares_taken, price = find_optimal_combination(itinerary, fares)
+    fares_taken, price = find_optimal_combination_iterative(itinerary, fares)
 
     answer = [fare.fid for fare in fares_taken]
+    # print(answer)
     with open(os.environ["RESULT_FILE"], 'w') as f:
         f.write(json.dumps(answer))
 
